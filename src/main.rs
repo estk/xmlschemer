91ed6c09e9aaa1f5ca8ea6e66f1644684ac55625
Evan Simmons <esims89@gmail.com>
2019-01-22 22:05:21 -0800

    Formatted working code

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,21 @@ extern crate quote;
 
 use clap::{app_from_crate, Arg};
 use log::debug;
-use serde_xml_rs::from_reader;
+use rustfmt_nightly::{Config, Input, Session};
+use serde_xml_rs::{from_reader, from_str};
+use std::env;
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::sink;
+use std::io::Write;
+use std::io::{BufReader, BufWriter};
+use std::path::PathBuf;
+use xmlschemer::schema::CodeGenerator;
+use xmlschemer::schema::{Context, Schema};
 
-fn main() {
+const PRIMITIVE_SCHEMA: &str = include_str!("./assets/primitives.xsd");
+
+fn main() -> Result<(), Box<dyn Error>> {
     let matches = app_from_crate!()
         .arg(
             Arg::with_name("in")
@@ -38,12 +47,36 @@ fn main() {
     let in_file = File::open(in_filename).expect("Unable to open in file");
 
     let out_filename = matches.value_of("out").expect("Output file is required");
+    let out_path = PathBuf::from(out_filename);
     debug!("opening output file: {}", out_filename);
-    let out_file = File::open(out_filename).expect("Unable to open out file");
+    let out_file = File::create(out_filename).expect("Unable to open out file");
 
-    // let in_reader = BufReader::new(in_file);
-    // let data = read(in_reader)?;
-    //
-    // let out_writer = BufWriter::new(out_file);
-    // out_writer.write_all(data.as_rust());
+    let reader = BufReader::new(in_file);
+    let schema: Schema = from_reader(reader)?;
+    let schema_source = schema.codegen(&mut Context::default());
+
+    let primitives: Schema = from_str(PRIMITIVE_SCHEMA)?;
+    let primitives_source = primitives.codegen(&mut Context::default());
+
+    let source = quote!(
+        mod xsd {
+            #primitives_source
+        }
+
+        #schema_source
+    );
+    let source_string = source.to_string();
+
+    let mut out_writer = BufWriter::new(out_file);
+    out_writer.write_all(&source_string.as_bytes())?;
+    out_writer.flush()?;
+    drop(out_writer);
+
+    let mut output = sink();
+    let fmt_res = Session::new(Config::default(), Some(&mut output))
+        .format(Input::File(PathBuf::from(out_filename)))
+        .map_err(|_| "Error formatting")?;
+
+    println!("result: {}", fmt_res);
+    Ok(())
 }
--- tests/gen/gpx.rs
+++ tests/gen/gpx.rs
@@ -1,6 +1,183 @@
+mod xsd {
+    use serde_derive::{Deserialize, Serialize};
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct String(std::string::String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Boolean(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Float(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Double(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Decimal(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct DateTime(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Duration(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct HexBinary(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Base64Binary(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct AnyUri(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Id(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Idref(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Entity(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Notation(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct NormalizedString(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Token(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Language(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Idrefs(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Entities(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Nmtoken(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Nmtokens(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Name(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct QName(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct NcName(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Integer(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct NonNegativeInteger(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct PositiveInteger(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct NonPositiveInteger(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct NegativeInteger(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Byte(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Int(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Long(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Short(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct UnsignedByte(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct UnsignedInt(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct UnsignedLong(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct UnsignedShort(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Date(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct Time(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct GYearMonth(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct GYear(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct GMonthDay(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct GDay(String);
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    #[serde(transparent)]
+    pub struct GMonth(String);
+}
 use serde_derive::{Deserialize, Serialize};
-include!("./primitives.rs");
-
 #[doc = " GPX is the root element in the XML file."]
 #[serde(rename = "gpx")]
 #[derive(Serialize, Deserialize, Debug)]
@@ -20,22 +197,27 @@ pub enum GpxTypeBody {
     Trk(TrkType),
     Extensions(ExtensionsType),
 }
+#[doc = " The latitude of the point.  Decimal degrees, WGS84 datum."]
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(transparent)]
 pub struct LatitudeType(String);
+#[doc = " The longitude of the point.  Decimal degrees, WGS84 datum."]
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(transparent)]
 pub struct LongitudeType(String);
+#[doc = " Used for bearing, heading, course.  Units are decimal degrees, true (not magnetic)."]
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(transparent)]
 pub struct DegreesType(String);
+#[doc = " Type of GPS fix.  none means GPS had no fix.  To signify \"the fix info is unknown, leave out fixType entirely. pps = military signal used"]
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(transparent)]
 pub struct FixType(String);
+#[doc = " Represents a differential GPS station."]
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(transparent)]
